e1ee1d2893882a6315b889d5f719fdcf47263feb
TNTrol <sem.go@bk.ru>
2022-01-24 15:31:23 +0300

    add keycloak security

--- src/main/java/ru/redcollar/notification/config/SecurityConfig.java
+++ src/main/java/ru/redcollar/notification/config/SecurityConfig.java
@@ -1,40 +1,41 @@
 package ru.redcollar.notification.config;
 
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
-import ru.redcollar.notification.component.JwtFilter;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-import java.util.Properties;
-
-@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
-@RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-    private final JwtFilter jwtFilter;
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        KeycloakAuthenticationProvider kap = keycloakAuthenticationProvider();
+        kap.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(kap);
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
-                .and()
-                .addFilterAfter(jwtFilter, SecurityContextPersistenceFilter.class)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf().disable();
     }
--- src/main/java/ru/redcollar/notification/controller/MailController.java
+++ src/main/java/ru/redcollar/notification/controller/MailController.java
@@ -2,10 +2,8 @@ package ru.redcollar.notification.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.redcollar.notification.domain.Mail;
 import ru.redcollar.notification.service.EmailService;
@@ -16,9 +14,8 @@ public class MailController {
 
     private final EmailService emailService;
 
-    @PreAuthorize("hasRole('ROLE_CLIENT_MAIL')")
     @PostMapping("/email")
-    public ResponseEntity<Void> sendMail(@RequestBody Mail mail){
+    public ResponseEntity<Void> sendMail(@RequestBody Mail mail) {
         emailService.sendEmail(mail);
         return ResponseEntity.ok().build();
     }
